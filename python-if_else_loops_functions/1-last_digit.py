c1f341116b4bd85e09ff6d45b56531cc9e18f326
lscblack <l.christian@alustudent.com>
2023-10-28 16:18:52 +0200

    my first python commit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ last_digit = abs(number) % 10  # Get the last digit of the absolute value of the
 print("Last digit of", number, "is", last_digit, end=" ")
 
 if last_digit > 5:
-    print("and is greater than 5")
+    print("and is greater than 5 and not 0")
 elif last_digit == 0:
     print("and is 0")
 else:

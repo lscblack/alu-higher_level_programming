fd4d8f1b3e14831f52a71c19f59a98fb7203540b
lscblack <l.christian@alustudent.com>
2023-10-28 17:43:15 +0200

    my first python commit second project

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def islower(c):
+    # Check if the ASCII value of c is in the range of lowercase letters
+    return ord('a') <= ord(c) <= ord('z')
+
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            # If the character is lowercase, convert it to uppercase using ASCII values
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print(uppercase_char, end='')
+        else:
+            # If the character is not lowercase, print it as is
+            print(char, end='')
+
+    # Print a new line at the end
+    print()

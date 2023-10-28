2f2eaed68fbe0665ca5cfa466e483f405df231d7
lscblack <l.christian@alustudent.com>
2023-10-28 18:15:32 +0200

    my first python commit second project

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('Z') - 1, -1):
-    if (ord('a') <= char <= ord('z')) or (ord('A') <= char <= ord('Z')):
+    if (ord('a') <= char <= ord('z')) or (ord('A') <= char < ord('Z')):
         if char % 2 != 0:
             print("{}".format(chr(char).upper()), end='')
         else:

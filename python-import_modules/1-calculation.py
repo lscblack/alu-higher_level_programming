d2a1fb4dcda0c2dc2cd23cdbf89d8e6918e51c30
lscblack <l.christian@alustudent.com>
2023-10-28 18:25:25 +0200

    my first python commit third project

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div)

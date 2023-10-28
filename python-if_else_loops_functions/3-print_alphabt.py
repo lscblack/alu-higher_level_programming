2af493223574d37c9721c00afe4345428a591c01
lscblack <l.christian@alustudent.com>
2023-10-28 17:09:26 +0200

    my first python commit second project

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for let in range(ord('a'), ord('z')+1):
     if chr(let) == 'e' or chr(let) == 'q':
-     continue
+        continue
     else:
-     print("{}".format(chr(let)), end='')
+        print("{}".format(chr(let)), end='')

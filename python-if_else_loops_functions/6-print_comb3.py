825c9614c918ee925109d0aede96461bedf0c35b
lscblack <l.christian@alustudent.com>
2023-10-28 17:46:34 +0200

    my first python commit second project

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " if tens_digit < 8 else "\n")
+for d in range(10):
+    for a in range(d + 1, 10):
+        print("{:d}{:d}".format(d, a), end=", " if d < 8 else "\n")

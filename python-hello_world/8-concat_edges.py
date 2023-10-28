b7eaa41b87a0e0b3978545bc6d7366fd40700797
lscblack <l.christian@alustudent.com>
2023-10-28 14:37:29 +0200

    my first python commit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = """Python is an interpreted, interactive, object-oriented programming
  language that combines remarkable power with very clear syntax"""
-print(str)
+print(str[43:67])

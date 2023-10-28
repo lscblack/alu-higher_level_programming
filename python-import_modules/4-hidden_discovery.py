88af79776b7544cfcc6f0ce33cae280e574f1dbd
lscblack <l.christian@alustudent.com>
2023-10-28 19:12:15 +0200

    my first python commit third project

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,26 @@
-#!/usr/bin/python3.8
+import sys
 
-import py_compile
-import marshal
+# Prevent the program from being executed when imported
+if __name__ != "__main__":
+    sys.exit(0)
 
-def main():
-    compiled_code = open("hidden_4.pyc", "rb").read()
-    code = marshal.loads(compiled_code[12:])
-    names = code.co_names
+# Load the compiled module
+try:
+    import imp
+    hidden_4 = imp.load_source("hidden_4", "hidden_4.pyc")
+except ImportError:
+    print("Failed to load hidden_4.pyc")
+    sys.exit(1)
 
-    for name in sorted(names):
-        if not name.startswith('__'):
-            print(name)
+# Get all the names defined in the module
+names = dir(hidden_4)
 
-if __name__ == "__main__":
-    main()
+# Filter out the names starting with __
+filtered_names = [name for name in names if not name.startswith("__")]
+
+# Sort the filtered names in alphabetical order
+sorted_names = sorted(filtered_names)
+
+# Print each name on a new line
+for name in sorted_names:
+    print(name)

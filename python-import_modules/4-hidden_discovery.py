2ea4ff0f6ae11c9317d7fbe82afb959db14dc310
lscblack <l.christian@alustudent.com>
2023-10-28 19:06:53 +0200

    my first python commit third project

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
+
 import py_compile
 import marshal
 
-if __name__ == "__main__":
+if __name__ == "__main":
     compiled_code = open("hidden_4.pyc", "rb").read()
     code = marshal.loads(compiled_code[12:])
     names = code.co_names

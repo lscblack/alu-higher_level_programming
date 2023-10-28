1ac45f1260d704780b77322ab4ca07bd2d225b4b
lscblack <l.christian@alustudent.com>
2023-10-28 18:52:22 +0200

    my first python commit third project

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,10 @@ from sys import argv
 argc = len(argv) - 1
 args = argv[1:]
 
-print("Number of argument{}: {}{}".format("s" if argc != 1 else "", argc, "." if argc == 0 else ":"))
+if argc == 0:
+    print("0 argument{}.".format("" if argc == 1 else "s"))
+else:
+    print("{} argument{}:".format(argc, "" if argc == 1 else "s"))
 
-if argc > 0:
-    for i, arg in enumerate(args, 1):
-        print("{}: {}".format(i, arg))
+for i in range(argc):
+    print("{}: {}".format(i + 1, args[i]))

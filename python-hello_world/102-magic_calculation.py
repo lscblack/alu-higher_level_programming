d0125c3c17b67d6115e472efa0de4dcd53ba0835
lscblack <l.christian@alustudent.com>
2023-10-28 15:54:08 +0200

    my first python commit

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+import dis
+
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result = result ** a  # LOAD_FAST 0 (a) and BINARY_POWER
-    result += b  # LOAD_FAST 1 (b) and BINARY_ADD
-    return result  # RETURN_VALUE
+    return 98 + (a ** b)

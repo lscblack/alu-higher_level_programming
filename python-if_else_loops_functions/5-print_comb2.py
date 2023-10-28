d2b846a53150273d41d2b33aa5c60d8b2046cb7c
lscblack <l.christian@alustudent.com>
2023-10-28 17:15:38 +0200

    my first python commit second project

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 99 + 1):
     if num == 99:
-        print("{:02}".format(num), end="")
+        print("{:02}".format(num), end=" ")
     else:
         print("{:02}".format(num), end=", ")

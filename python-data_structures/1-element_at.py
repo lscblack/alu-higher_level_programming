440eff442ce1f35d51aa6d26eecad4f83554a3f6
lscblack <l.christian@alustudent.com>
2023-11-04 09:38:21 +0200

    python datastructures lsc

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return "None"
-    elif idx > len(my_list):
-        return "None"
-    elif idx >=0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         return my_list[idx]
+    else:
+        return "None"
     
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    elif idx >= 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
+        return my_list
+    else:
         return my_list

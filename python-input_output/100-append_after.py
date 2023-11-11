e02093d10812cb28b972a2dd30b886cd8ccf3421
lscblack <l.christian@alustudent.com>
2023-11-11 13:50:08 +0200

    python more

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""append_after"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """append_after"""
-
-    with open(filename, mode="r+", encoding="utf-8") as readFile:
-        data = readFile.readlines()
-
-    count = 0
-    with open(filename, mode="w", encoding="utf-8") as writeFile:
-        for lines in data:
-            count += 1
-            if search_string in lines:
-                data.insert(count, new_string)
-        for lines in data:
-            writeFile.write(lines)
+    """Insert text after each line containing a given string."""
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)

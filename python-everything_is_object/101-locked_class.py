867bb680360889081dd3905dedbb25a1f5ec6d03
Loue Sauveur Christian <141139366+lscblack@users.noreply.github.com>
2023-11-13 12:38:23 +0200

    Update 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,9 +3,6 @@
 
 
 class LockedClass:
-    """
-    prevent the user from instantiating new lockedClass attributes
-    for anything but attributes called 'first_namer'.
-    """
+
 
     __slots__ = ["first_name"]

2455ed4809fe6ab5b69e3af25f2fb15907e02fef
Loue Sauveur Christian <141139366+lscblack@users.noreply.github.com>
2023-11-13 12:37:20 +0200

    Update 100-singly_linked_list.py

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -6,11 +6,7 @@ class Node:
     """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
+
         self.data = data
         self.next_node = next_node
 

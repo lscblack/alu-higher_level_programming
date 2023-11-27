c216f30de4596d7f349e56a808ac527101f5bb2b
Loue Sauveur Christian <l.christian@alustudent.com>
2023-11-27 09:33:18 -0800

    errors solved

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- cc
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

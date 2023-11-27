3c09db2cff595a9224956783fd886819183b674a
lscblack <l.christian@alustudent.com>
2023-11-27 15:46:57 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- privallages
-SHOW GRANTS FOR  'user_0d_1' @'localhost';
-SHOW GRANTS FOR  'user_0d_2' @'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

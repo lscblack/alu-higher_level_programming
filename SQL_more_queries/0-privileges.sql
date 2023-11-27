e2cade94b7dc0fe073cb05032a1d99ce334c5012
lscblack <l.christian@alustudent.com>
2023-11-27 19:48:05 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
--- Check if user_0d_1 exists and display its grants
-SELECT COUNT(*) AS user_0d_1_exists FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
+-- Check if user_0d_1 exists 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Check if user_0d_2 exists and display its grants
-SELECT COUNT(*) AS user_0d_2_exists FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

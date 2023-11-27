87e94256c42cb4db839feb310ddadb3ef7fc74af
lscblack <l.christian@alustudent.com>
2023-11-27 18:59:16 +0200

    SQL_more_queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,3 @@
 -- Check if user_0d_1 exists
-SELECT 
-    GROUP_CONCAT(privilege_type) AS privileges
-FROM 
-    information_schema.USER_PRIVILEGES 
-WHERE 
-    GRANTEE = "'user_0d_1'@'localhost'";
-
-
--- Check if user_0d_2 exists
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';

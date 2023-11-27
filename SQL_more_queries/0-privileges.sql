20dac786e56ce05921c7a9c9f7b6183922450fbe
lscblack <l.christian@alustudent.com>
2023-11-27 18:16:03 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,6 @@
 -- list privileges
 CREATE USER 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 

5c7c4854b151119fc8348e64944c287de9bf5ccc
lscblack <l.christian@alustudent.com>
2023-11-27 18:00:39 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list privileges
-SELECT * FROM information_schema.USER_PRIVILEGES WHERE GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

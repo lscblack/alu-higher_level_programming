5db4c35d5291e57885a661a270e3b149ce79b2b4
lscblack <l.christian@alustudent.com>
2023-11-27 18:41:52 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,15 @@
--- Check if user_0d_1 exists
-SELECT 
-    GROUP_CONCAT(privilege_type) AS privileges
-FROM 
-    information_schema.USER_PRIVILEGES 
-WHERE 
-    GRANTEE = "'user_0d_1'@'localhost'";
-
-
--- Check if user_0d_2 exists
-
-SELECT 
-    GROUP_CONCAT(privilege_type) AS privileges
-FROM 
-    information_schema.USER_PRIVILEGES 
-WHERE 
-    GRANTEE = "'user_0d_2'@'localhost'";
+-- select
+SELECT
+    CONCAT(
+        'GRANT ',
+        GROUP_CONCAT(privilege_type SEPARATOR ', '),
+        ' ON *.* TO ',
+        QUOTE(GRANTEE),
+        ';'
+    ) AS privilege_statement
+FROM
+    information_schema.USER_PRIVILEGES
+WHERE
+    GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'")
+GROUP BY
+    GRANTEE;

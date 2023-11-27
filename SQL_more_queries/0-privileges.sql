d0680731c0e01c61481c8efe1d79cc8fef6068c1
lscblack <l.christian@alustudent.com>
2023-11-27 18:15:09 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,21 +1,7 @@
 -- list privileges
--- Initial attempt to show privileges, expecting an error
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Creating user user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Granting all privileges to user_0d_1 on all databases
+CREATE USER 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Show privileges for user_0d_1
+CREATE USER 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Attempting to show privileges for user_0d_2, expecting an error
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- Creating user user_0d_1 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
--- Granting all privileges to user_0d_1 on all databases
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';

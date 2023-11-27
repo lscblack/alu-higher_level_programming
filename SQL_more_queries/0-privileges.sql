c9bb8ce1d0d11588d4668d16f218aaec81550823
lscblack <l.christian@alustudent.com>
2023-11-27 18:37:54 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,11 @@
 -- Check if user_0d_1 exists
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SELECT 
+    GROUP_CONCAT(privilege_type) AS privileges
+FROM 
+    information_schema.USER_PRIVILEGES 
+WHERE 
+    GRANTEE = "'user_0d_1'@'localhost'";
+
 
 -- Check if user_0d_2 exists
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
--- cc
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.*
+	TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

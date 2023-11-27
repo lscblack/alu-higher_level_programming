781cad3a9495b2a8a4e773f30a447212806294f5
lscblack <l.christian@alustudent.com>
2023-11-27 17:53:16 +0200

    Sql Introduction

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- list privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- cc
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

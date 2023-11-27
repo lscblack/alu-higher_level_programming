8e5f0afbf563c092519b3301df7d3c804431fcb0
lscblack <l.christian@alustudent.com>
2023-11-27 18:39:13 +0200

    Sql Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -8,4 +8,10 @@ WHERE
 
 
 -- Check if user_0d_2 exists
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+SELECT 
+    GROUP_CONCAT(privilege_type) AS privileges
+FROM 
+    information_schema.USER_PRIVILEGES 
+WHERE 
+    GRANTEE = "'user_0d_2'@'localhost'";

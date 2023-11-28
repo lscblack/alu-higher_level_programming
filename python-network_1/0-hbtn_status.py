995cba86abcfeffa2497a98436ed044d2323a8e4
lscblack <l.christian@alustudent.com>
2023-11-28 19:59:08 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -17,4 +17,5 @@ if __name__ == '__main__':
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content.decode("utf-8")))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))

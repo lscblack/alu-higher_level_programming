ecc8012476251c04d09b6668866a3b37c84b5a1c
lscblack <l.christian@alustudent.com>
2023-11-28 20:16:35 +0200

    sq

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,12 +8,7 @@ It fetches content from 'https://intranet.hbtn.io/status' using urllib and displ
 import urllib.request
 
 if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    req = urllib.request.Request(url, headers=headers)
-    
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))

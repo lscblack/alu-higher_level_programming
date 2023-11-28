52409a50353bffed813932022db1b3b5d6695b39
lscblack <l.christian@alustudent.com>
2023-11-28 19:50:25 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,15 +8,8 @@ It fetches content from 'https://intranet.hbtn.io/status' using urllib and displ
 import urllib.request
 
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    
-    try:
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTPError: {e.code} - {e.reason}")
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content.decode("utf-8")))

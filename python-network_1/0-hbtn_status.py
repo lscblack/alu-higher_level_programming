0aa5eb317e2b92160ee4271e32c73b735f675a1c
lscblack <l.christian@alustudent.com>
2023-11-28 20:57:04 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,22 @@
-#!/usr/bin/python3
-"""
-documented by no
-"""
-
 import urllib.request
 
 if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
+    url1 = 'https://alu-intranet.hbtn.io/status'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     
-    req = urllib.request.Request(url, headers=headers)
+    req = urllib.request.Request(url1, headers=headers)
     
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        print("Body response:")
+        print("First Request - Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
 
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url2 = 'http://0.0.0.0:5050/status'
+    with urllib.request.urlopen(url2) as response:
         content = response.read()
-        print("Body response:")
+        print("\nSecond Request - Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))

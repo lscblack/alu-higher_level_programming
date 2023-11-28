782aa29f703a9f17a51d2f98149f26f42169f668
lscblack <l.christian@alustudent.com>
2023-11-28 21:47:47 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+    'User-Agent': 
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
 }
 
 req = urllib.request.Request(url, headers=headers)

a06b40c67bff23f753d307a996670e789d4ba4f2
lscblack <l.christian@alustudent.com>
2023-11-28 21:17:44 +0200

    sa

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,25 +1,26 @@
  #!/usr/bin/python3
 
-""" docuemhjf """
 import urllib.request
-""" docuemhjf """
-if __name__ == '__main__':
-    url1 = 'https://alu-intranet.hbtn.io/status'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    req = urllib.request.Request(url1, headers=headers)
-    
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("First Request - Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
 
-    url2 = 'http://0.0.0.0:5050/status'
-    with urllib.request.urlopen(url2) as response:
+def fetch_and_display(url, expected_output):
+    with urllib.request.urlopen(url) as response:
         content = response.read()
-        print("\nSecond Request - Body response:")
+        print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        if content.decode("utf-8") == expected_output:
+            print("(Expected output matched)")
+        else:
+            print("(Expected output NOT matched)")
+
+if __name__ == '__main__':
+    # Fetching 'https://intranet.hbtn.io/status'
+    url_1 = 'https://intranet.hbtn.io/status'
+    expected_output_1 = 'OK'
+    fetch_and_display(url_1, expected_output_1)
+
+    # Fetching 'http://0.0.0.0:5050/status'
+    url_2 = 'http://0.0.0.0:5050/status'
+    expected_output_2 = 'Custom status'
+    fetch_and_display(url_2, expected_output_2)

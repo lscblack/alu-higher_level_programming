a98353c8fea0abab118c59a082eb35d7a5f6aa34
lscblack <l.christian@alustudent.com>
2023-11-28 20:31:16 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -12,3 +12,15 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    # Second request with headers
+    url = 'https://alu-intranet.hbtn.io/status'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    req = urllib.request.Request(url, headers=headers)
+    
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("\nBody response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))

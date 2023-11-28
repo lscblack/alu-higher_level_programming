8cd8fb9f301ac37fe69b55b74280baecae0348ef
lscblack <l.christian@alustudent.com>
2023-11-28 18:04:32 +0200

    sql

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python3
-# modulev imported
+#!/usr/bin/env python3
+"""
+Python script using urllib to fetch content from https://alu-intranet.hbtn.io/status
+and display the response body with tabulation before each line.
+"""
+
 import urllib.request
 
+# URL to fetch
 url = 'https://alu-intranet.hbtn.io/status'
 
+# Creating a request object with the URL
 req = urllib.request.Request(url)
 
+# Making the request and reading the response body
 with urllib.request.urlopen(req) as response:
     body = response.read().decode('utf-8')
 

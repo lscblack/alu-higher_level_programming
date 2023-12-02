833bae57df287c553571d29ee304dc35b71027df
lscblack <l.christian@alustudent.com>
2023-12-02 11:14:18 +0200

    fgjmb

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,30 +1,75 @@
 #!/usr/bin/python3
-""" Unittest for Base Class."""
+"""Test case"""
 
-
+import os
 import unittest
+
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    """Test for Base class."""
+    """Test class"""
 
-    def setUp(self) -> None:
-        """Set up the base class."""
-        pass
+    def test_basic(self):
+        """Test basic func"""
+        base = Base()
+        base_1 = Base()
+        base_89 = Base(89)
+        self.assertEqual(base.id, 1)
+        self.assertEqual(base_1.id, 2)
+        self.assertEqual(base_89.id, 89)
 
-    def tearDown(self) -> None:
-        """Tear down the base class."""
-        pass
+    def test_to_json_string(self):
+        """Test Func"""
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+        self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
 
-    def test_id(self):
-        """Test id"""
+    def test_from_json_string(self):
+        """Test Func"""
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string("[]"), [])
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+        self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
+
+    def test_save_to_file(self):
+        """Test func"""
         Base._Base__nb_objects = 0
-        b1 = Base()
-        b2 = Base()
-        b3 = Base(98)
-        b4 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 98)
-        self.assertEqual(b4.id, 3)
+
+        Square.save_to_file(None)
+
+        self.assertTrue(os.path.isfile("Square.json"))
+
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+
+        Square.save_to_file([])
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+            self.assertEqual(type(fname.read()), str)
+
+        Square.save_to_file([Square(1)])
+        with open("Square.json") as fname:
+            self.assertEqual(fname.read(),
+                             '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+        Base._Base__nb_objects = 0
+
+        Rectangle.save_to_file(None)
+        self.assertTrue(os.path.isfile("Rectangle.json"))
+        
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+
+        Rectangle.save_to_file([])
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(), '[]')
+            self.assertEqual(type(fname.read()), str)
+
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        with open("Rectangle.json") as fname:
+            self.assertEqual(fname.read(),
+                             '[{"id": 1, "width": 1, '
+                             '"height": 2, "x": 0, "y": 0}]')
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -6,15 +6,15 @@ import unittest
 from io import StringIO
 from unittest.mock import patch
 
-from base import Base
-from square import Square
+from models.base import Base
+from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
     """Test for class Square"""
 
     def test_instance(self):
-        """Doc"""
+        """Test the functions in square"""
         s = Square(1)
         s1 = Square(1, 2)
         s2 = Square(1, 2, 3)
@@ -48,12 +48,12 @@ class TestSquare(unittest.TestCase):
             s6 = Square(1, 2, "3")
 
     def test_area(self):
-        """Doc"""
+        """Test the functions in square"""
         s1 = Square(2)
         self.assertEqual(s1.area(), 4)
 
     def test__str__(self):
-        """Doc"""
+        """Test the functions in square"""
         Base._Base__nb_objects = 0
         s1 = Square(2)
         with patch("sys.stdout", new=StringIO()) as fake_out:
@@ -62,7 +62,7 @@ class TestSquare(unittest.TestCase):
                              "[Square] (1) 0/0 - 2\n")
 
     def test_display(self):
-        """Doc"""
+        """Test the functions in square"""
         s1 = Square(2)
         s2 = Square(2, 2, 3)
         with patch("sys.stdout", new=StringIO()) as fake_out:
@@ -75,14 +75,14 @@ class TestSquare(unittest.TestCase):
                              "\n\n\n  ##\n  ##\n")
 
     def test_to_dictionary(self):
-        """Doc"""
+        """Test the functions in square"""
         Base._Base__nb_objects = 0
         s1 = Square(4)
         self.assertEqual(s1.to_dictionary(),
                          {'id': 1, 'size': 4, 'x': 0, 'y': 0})
 
     def test_update(self):
-        """Doc"""
+        """Test the functions in square"""
         Base._Base__nb_objects = 0
         s1 = Square(2)
         s1.update()
@@ -125,7 +125,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1.y, 3)
 
     def test_create(self):
-        """Doc"""
+        """Test the functions in square"""
 
         s1 = Square.create(**{'id': 89})
         self.assertEqual(s1.id, 89)
@@ -154,7 +154,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1.y, 3)
 
     def test_save_to_file(self):
-        """Doc"""
+        """Test the functions in square"""
         Base._Base__nb_objects = 0
 
         Square.save_to_file(None)
@@ -180,7 +180,7 @@ class TestSquare(unittest.TestCase):
             self.assertEqual(type(file.read()), str)
 
     def test_load_from_file(self):
-        """Doc"""
+        """Test the functions in square"""
         if os.path.exists("Square.json"):
             os.remove("Square.json")
 
@@ -189,4 +189,3 @@ class TestSquare(unittest.TestCase):
         from_file = Square.load_from_file()
         self.assertEqual(type(from_file), list)
         self.assertEqual(from_file[0].size, 2)
-
